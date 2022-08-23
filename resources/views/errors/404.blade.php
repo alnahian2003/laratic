06666281faa5c29417fb5dbc2c1331ce4da6e586
alnahian2003 <a.alnahian2003@gmail.com>
2022-08-23 16:29:36 +0600

    custom 404 error page updated

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,2 +1,16 @@
-<title>404 Not Found</title>
-<img width="50%" height="auto" src="https://i.imgflip.com/34is30.jpg" alt="{{$exception->getMessage()}}">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/js/app.js')
+    <title>404 Not Found</title>
+</head>
+<body class="p-2 text-center">
+    <figure class="mt-8">
+        <img class="mx-auto max-w-lg rounded-lg" src="https://i.imgflip.com/34is30.jpg" alt="404 Not Found">
+    </figure>
+    <button class="my-8 btn btn-success" onclick="window.location='{{ route('index') }}'">🔙 Return to Home</button>
+</body>
+</html>
