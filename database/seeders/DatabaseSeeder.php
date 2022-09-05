40125074c36d9809e0f20527dff64fa822a313ed
alnahian2003 <a.alnahian2003@gmail.com>
2022-09-05 23:39:02 +0600

    PostSeeder created in the BD Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        \App\Models\Post::factory(100)->create();
+        // \App\Models\Post::factory(100)->create();
+
+        $this->call([
+            PostSeeder::class,
+        ]);
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
