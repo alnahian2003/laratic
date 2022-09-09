f9278460ff4007ef04171e76d48308db8f88e528
alnahian2003 <a.alnahian2003@gmail.com>
2022-09-09 21:07:39 +0600

    Retrieving Single Models/Aggregates chapter completed

--- routes/web.php
+++ routes/web.php
@@ -85,10 +85,80 @@ Route::get('/', [PostController::class, 'index']);
 
 Route::get('db', function () {
 
-    foreach (App\Models\Post::lazy() as $post) {
-        print_r($post);
-    }
+    // foreach (App\Models\Post::cursor() as $post) {
+    //     echo "<pre>";
+    //     print_r($post);
+    //     echo "</pre>";
+    // }
+
+    // Retrieve a model by its primary key...
+    // return App\Models\User::find(151);
+
+    // Retrieve the first model matching the query constraints...
+    // return App\Models\User::where('username', 'alnahian2003')->first();
+
+
+    // Alternative to retrieving the first model matching the query constraints...
+    // return App\Models\User::firstWhere('username', request('username'));
+
+    // return App\Models\Post::findOr(10, fn () => abort(404));
+
+    // return App\Models\User::findOr(1, ['username', 'name', 'created_at'], fn () => abort(404));
+
+    // Get the first post from db where it has more than 9k views
+    // return App\Models\Post::where('views', '>', 9000)->firstOr(fn () => abort(404));
+
+    // Throw an exception if a model is not found
+    // return App\Models\Post::findOrFail(1000);
+
+    // return App\Models\User::firstOrFail(['username', 'email']);
+
+    // Retrieving Or Creating Models
+    // return App\Models\User::firstOrCreate(
+    //     ['username' => 'bond007'],
+    //     [
+    //         'name' => 'James Bond',
+    //         'email' => 'bond007@cia.com',
+    //         'password' => bcrypt('bond007')
+    //     ]
+    // );
+
+    // $user = App\Models\User::firstOrNew(
+    //     ['username' => 'feluda'],
+    //     [
+    //         'name' => 'Felu Mittir',
+    //         'email' => 'feluda@raw.com',
+    //         'password' => bcrypt('felumittir')
+    //     ]
+    // );
+
+    // $user->save();
+
+    // return $user;
+
+    // ### Retrieving Aggregates
+    return App\Models\Post::latest()
+        ->limit(10)
+        ->sum('views'); // get total views of the last 10 posts
+
+    // ###################
+    // return App\Models\Post::addSelect(["title" => App\Models\User::select('name')])->whereColumn('post_id', 'users.id')->limit(1)->dd();
+
 
     // return App\Models\Post::simplePaginate(5);
     // return DB::table('posts')->get()->dd();
+});
+
+
+Route::get('search/{search?}', function ($search = '') {
+    if ($search == '') {
+        return App\Models\User::limit(10)
+            ->latest()
+            ->get();
+    }
+
+    return App\Models\User::where('username', $search)
+        ->orWhere('id', $search)
+        ->latest()
+        ->firstOrFail();
 });
