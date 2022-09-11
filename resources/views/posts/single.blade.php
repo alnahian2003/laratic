c45f2f70316354a0b2044aa0f3e35c960aaccf27
alnahian2003 <a.alnahian2003@gmail.com>
2022-09-11 14:47:00 +0600

    auth check added for edit post button

--- resources/views/posts/single.blade.php
+++ resources/views/posts/single.blade.php
@@ -4,9 +4,12 @@
     <section id="post">
             <!-- Post -->
             <article class="post my-10 border-b border-b-white/20">
-                @if($post->user_id == auth()->user()->id)
+                @auth
+                    @if($post->user_id == auth()->user()->id)
                     <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-sm btn-outline">edit Post</a>
-                @endif
+                    @endif
+                @endauth
+                
 
                 <h1 class="text-5xl font-extrabold pb-5 text-transparent bg-clip-text bg-gradient-to-l from-sky-600 to-success border-b mb-5 border-b-white/10 break-words leading-normal">{{$post->title}}</h1>
 
