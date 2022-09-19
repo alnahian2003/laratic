44f7f06e16cd5b3889ab58c59039ae047b3ab884
alnahian2003 <a.alnahian2003@gmail.com>
2022-09-19 23:30:31 +0600

    Update related models when the Comment model gets updated

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -9,6 +9,16 @@ class Comment extends Model
 {
     use HasFactory;
 
+    /**
+     * All of the relationships to be touched.
+     * 
+     * When Comment model is updated, automatically "touch" the updated_at
+     * timestamp of the owning Post so that it is set to the current date and time.
+     * 
+     * @var array
+     */
+    protected $touches = ['post', 'user'];
+
     public function user()
     {
         return $this->belongsTo(User::class, 'user_id');
