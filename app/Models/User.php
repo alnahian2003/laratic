bdb4e5bb2bb7596cd3db1f1d1ad04ce3c8fd2c87
alnahian2003 <a.alnahian2003@gmail.com>
2022-09-25 00:20:45 +0600

    Customizing the pivot attr name

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,6 +58,7 @@ class User extends Authenticatable
     public function roles()
     {
         return $this->belongsToMany(Role::class)
+            ->as('role')
             ->withTimestamps();
     }
 }
