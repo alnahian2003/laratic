bfebd11623c77dc64fd0e4adabcd65cfb2b4b1d7
alnahian2003 <a.alnahian2003@gmail.com>
2022-11-15 19:07:22 +0600

    Global Scopes moved to App/Models dir

--- app/Models/Tweet.php
+++ app/Models/Tweet.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use App\Scopes\ActiveScope;
+use App\Models\Scopes\ActiveScope;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
